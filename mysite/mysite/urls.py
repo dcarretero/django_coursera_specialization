3514ed345931eb3cb1d37e61c8f2e8276a893fef
Daniel Carretero <daniel.carretero@gmail.com>
2024-11-14 23:02:46 +0100

    Modulo 4 curso 3 finalizado

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('autos/', include('autos.urls')),
+    path('cats/', include('cats.urls')),
     path('', TemplateView.as_view(template_name='home/main.html')),
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
